5da3a3c3e1dcaeb2bfc5fcbcdc3a035718ee7353
Neylaac <neyla.achaoui@student.ehb.be>
2026-06-08 02:35:32 +0200

    Create index.js
    Voeg basis Express-server toe

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/about", (req,res) =>{
+    res.send("About page")
+})
+
+app.get("/contact", (req,res) =>{
+    res.send("contact page")
+})
+
+
+app.listen(port, () => {
+    console.log(`App Listing on Port ${port}`);
+})
+
